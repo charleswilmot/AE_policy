c005e8471de04b0d7dce27d0a25ff986276c3475
Charles <charles.wilmot2@hotmail.fr>
2021-10-22 21:26:45 +0200

    initial commit

---
+++ src/experiment.py
@@ -0,0 +1,29 @@
+import hydra
+from procedure import Procedure
+import os
+import time
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+@hydra.main(config_path='../config/', config_name='defaults.yaml')
+def main(config):
+    with Procedure(config) as procedure:
+        n_training_steps_done = 0
+        n_iteration_collected = 0
+        last_time_logged = -1e7
+        while n_training_steps < config.n_training_steps:
+            procedure.collect_data(config.n_samples_collected_per_training)
+            n_iteration_collected += config.n_samples_collected_per_training
+            procedure.train(config.batch_size)
+            n_training_steps_done += 1
+            current_time = time.time()
+            if current_time - last_time_logged > config.log_every:
+                last_time_logged = current_time
+                print(f"{n_training_steps_done=}  {n_iteration_collected=}")
+        print("Experiment finished, hope it worked. Good bye!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/procedure.py
@@ -0,0 +1,66 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import models, layers, optimizers
+from simulation import SimulationPool, MODEL_PATH
+
+
+class Procedure:
+    def __init__(self, config):
+        self.encoder = models.Sequential([
+            layers.Dense(50, activation=tf.nn.relu),
+            layers.Dense(50, activation=tf.nn.relu),
+            layers.Dense(7, activation=tf.tanh),
+        ])
+        self.decoder = models.Sequential([
+            layers.Dense(50, activation=tf.nn.relu),
+            layers.Dense(50, activation=tf.nn.relu),
+            layers.Dense(7, activation=None),
+        ])
+        self.buffer = Buffer(config.buffer_size)
+        self.simulations = SimulationPool(
+            config.n_simulations,
+            scene=MODEL_PATH + '/custom_timestep.ttt',
+            guis=[0],
+        )
+        self.simulation_pool.set_simulation_timestep(0.2)
+        self.simulations.create_environment()
+        self.simulation_pool.set_reset_poses()
+        self.simulation_pool.set_control_loop_enabled(False)
+        self.simulation_pool.start_sim()
+        self.simulation_pool.step_sim()
+        print("[procedure] all simulation started")
+        self.optimizer = optimizers.Adam(config.learning_rate)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, tb):
+        self.close()
+
+    def close(self):
+        self.simulations.close()
+
+    def collect_data(self, n_iterations):
+        all_states = []
+        n_pool_iterations = n_iterations // self.simulations.size
+        if n_pool_iterations * self.simulations.size != n_iterations:
+            raise ValueError("Number of iterations not divisible by the Pool size")
+        for iteration in range(n_pool_iterations):
+            states = np.array(self.simulations.get_state())
+            all_states.append(states)
+            actions = self.encoder(states)
+            self.simulations.apply_action(actions)
+        self.buffer.integrate(np.concatenate(all_states))
+
+    def train(self, batch_size):
+        states = self.buffer.sample(batch_size)
+        with tf.GradientTape() as tape:
+            reconstructions = self.decoder(self.encoder(states))
+            loss = tf.reduce_sum(tf.reduce_mean((states - reconstructions) ^ 2, axis=-1))
+            vars = self.encoder.trainable_variables + self.decoder.trainable_variables
+            grads = tape.gradient(loss, vars)
+            self.optimizer.apply_gradients(zip(grads, vars))
+        return loss
+
+    def replay(self):
+        pass
